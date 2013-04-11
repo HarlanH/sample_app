723fc25337a9f258aceb1ef499c4283bd17bd1ee
Harlan Harris <harlan@harris.name>
2013-04-11 15:18:51 -0400

    through 7.3

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do 
+    factory :user do
+        name "Harlan Harris"
+        email "harlan.harris@gmail.com"
+        password "foobar"
+        password_confirmation "foobar"
+    end
+end
